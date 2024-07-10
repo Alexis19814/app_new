6dbe686a269757e3d758edee93aea02d1604101a
Alexis19814 <alexisdavid_96figueroa@hotmail.com>
2024-07-09 19:57:02 -0600

    creacion histograma

--- vehicles.py
+++ vehicles.py
@@ -1,3 +1,18 @@
 import streamlit as st 
 import pandas as pd
 import plotly.express as px
+
+st.header('Anuncio de coches en venta')
+
+car_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Construir histograma') # crear un botón
+        
+        if hist_button:
+            
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="price")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
